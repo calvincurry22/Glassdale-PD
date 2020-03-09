3d34e7a76d97d35f3a2fc7f61955c0b9f6da7b14
calvincurry22 <calvincurry22@gmail.com>
2020-03-09 15:34:44 -0500

    Added code to automate the info in the convictions dropdown button

---
+++ scripts/convictions/ConvictionSelect.js
@@ -0,0 +1,32 @@
+/*
+ *   ConvictionSelect component that renders a select HTML element
+ *   which lists all convictions in the Glassdale PD API
+ */
+import { useConvictions } from "./ConvictionProvider.js"
+
+// Get a reference to the DOM element where the <select> will be rendered
+const contentTarget = document.querySelector(".filters__crime")
+
+const ConvictionSelect = () => {
+    // Get all convictions from application state
+    const convictionsArray = useConvictions()
+
+    const render = convictionsCollection => {
+        /*
+            Use interpolation here to invoke the map() method on
+            the convictionsCollection to generate the option elements.
+            Look back at the example provided above.
+        */
+        contentTarget.innerHTML = `
+        <select class="dropdown" id="crimeSelect">
+            <option value="0">Please select a crime...</option>
+            ${convictionsArray.map(crime => {
+            return `<option>${crime.name}</option>`})
+        }
+            </select>`
+    }
+    
+    render(convictionsArray)
+}
+
+export default ConvictionSelect
--- scripts/main.js
+++ scripts/main.js
@@ -2,10 +2,15 @@ import { getOfficers } from './officers/OfficerProvider.js'
 import { useOfficers } from './officers/OfficerProvider.js'
 import { getCriminals } from './criminals/CriminalDataProvider.js'
 import CriminalList from './criminals/CriminalList.js'
+import ConvictionSelect from './convictions/ConvictionSelect.js'
+import { getConvictions } from './convictions/ConvictionProvider.js'
 
 
 getCriminals()
 .then(response => CriminalList()
     )
 
+getConvictions()
+    .then(response => ConvictionSelect()
+    )
     
